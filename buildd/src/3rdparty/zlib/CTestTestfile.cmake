e3bf037dc04e6af7212e900b511da55f2e9a9de0
Murii <muresanvladmihail@gmail.com>
2017-02-25 13:44:21 +0200

    removed garbage folder,ups

--- buildd/src/3rdparty/zlib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/murii/Desktop/Programming/CLove/src/3rdparty/zlib
-# Build directory: /home/murii/Desktop/Programming/CLove/build/src/3rdparty/zlib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
