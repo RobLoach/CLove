63234dad601a51e105854774aba7f569a9e49957
Rob Loach <robloach@gmail.com>
2017-07-22 11:15:11 -0400

    Use mkdir -p so that you can re-compile quickly

--- build_android.sh
+++ build_android.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 #Creates the lib used by android to make the .apk
-mkdir build_android
-cd build_android
+mkdir -p build_android &&
+cd build_android &&
 cmake     -BUILD_ANDROID=TRUE  \
 		  -DCMAKE_TOOLCHAIN_FILE="android.toolchain.cmake" \
 		  -DANDROID_NDK=${ANDROID_NDK} ../ &&
